4a4c74564d8871ab1be7f78c6f721c59b34c5bff
Xuheng Xao <caoxuheng@tongji.edu.cn>
2024-02-04 23:10:43 +0800

    Create __init__.py

---
+++ Networks/__init__.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def model_generator(method:str, device="cuda"):
+
+
+    if 'PKLNet' in method:
+        from .PKLNet.PKLNet import PKLNet
+        from .PKLNet.Config import args as opt
+        num_iterations = int(method.split('_')[-1])
+        model = PKLNet(sf=opt.sf,in_c=opt.inchannel, n_feat=opt.n_feat, nums_stages=num_iterations - 1,n_depth=opt.n_depth).to(device)
+    elif 'PSRT' in method:
+        from .PSRT.net import PSRTnet
+        from  .PSRT.Config import args_parser
+        opt = args_parser()
+        model = PSRTnet(opt).to(device)
+    elif 'MSST' in method:
+        from .MSST.net import Net
+        from .MSST.Config import argsParser
+        opt = argsParser()
+        model = Net(opt).to(device)
+    elif 'MoGDCN' in method:
+        from .MoGDCN.net import VSR_CAS
+        from .MSST.Config import argsParser
+        opt = argsParser()
+        model = VSR_CAS(opt).to(device)
+    elif 'Fusformer' in method :
+        from .Fusformer.net import MainNet
+
+        from .PKLNet.Config import args as opt
+        model = MainNet(sf=32, channel=128).to(device)
+    elif 'HyMS' in method:
+        from .HyMS.config import args
+        from .HyMS.main_gpu import HyMS
+
+        model =HyMS(args)
+        opt= args
+    elif 'UDALN' in method:
+        from .UDALN.net import udaln
+        from .UDALN.config import args as opt
+
+        # sp_range = [list(range(30)),list(range(13,50)),list(range(41,84)),list(range(68,128))]
+        sp_range = np.array([[0,30],[13, 50], [41, 84], [68, 127]])
+
+
+        model = udaln(opt,sp_range)
+    else:
+        print(f'opt.Method {method} is not defined !!!!')
+
+    return model, opt
