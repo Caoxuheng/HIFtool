0a24e52f339998d84c6ae476a50eba8816d3ef41
Xuheng Xao <caoxuheng@tongji.edu.cn>
2024-02-04 23:06:54 +0800

    Update config.py

--- Networks/FeafusFormer/config.py
+++ Networks/FeafusFormer/config.py
@@ -2,7 +2,7 @@ import argparse
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--msi_channel',type=int,default=1, help='spectral band number of MSI')
 parser.add_argument('--hsi_channel',type=int,default=4, help='spectral band number of HSI')
-parser.add_argument('--size',type=list,default=[1,4,1600,1600], help='')
+parser.add_argument('--size',type=list,default=[1,4,1600,1600], help='shape of the target image')
 parser.add_argument('--K',type=int,default=3, help='Hyperparameter of subspace-based loss function')
 parser.add_argument('--isCal_PSF',type=bool,default=True, help='if blind spatial degradation')
 parser.add_argument('--isCal_SRF',type=bool,default=True, help='if blind spectral degradation')
