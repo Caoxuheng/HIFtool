549f9aef40de08e40889af32893da36aff212275
Xuheng Cao <caoxuheng@tongji.edu.cn>
2024-05-17 12:50:05 +0800

    Update config.py

--- Networks/FeafusFormer/config.py
+++ Networks/FeafusFormer/config.py
@@ -1,14 +1,12 @@
 import argparse
 parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
-parser.add_argument('--sf', type=int, default=32, help="super resolution upscale factor")
-parser.add_argument('--patch_size',type=int,default=96, help='patch size for training')
-parser.add_argument('--hsi_channel', type=int, default=128, help='output channel number')
-parser.add_argument('--msi_channel', type=int, default=4, help='output channel number')
-parser.add_argument('--alpha', type=float, default=0.1, help='alpha')
-parser.add_argument('--nEpochs', type=int, default=0, help='number of epochs to train for')
-parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
-parser.add_argument('--mode', default=1, type=int, help='Train or Test.')
-parser.add_argument('--local_rank', default=1, type=int, help='None')
-parser.add_argument('--use_distribute', type=int, default=1, help='None')
+parser.add_argument('--sf', type=int, default=4, help="super resolution upscale factor")
+parser.add_argument('--hsi_channel', type=int, default=4, help='output channel number')
+parser.add_argument('--msi_channel', type=int, default=1, help='output channel number')
+parser.add_argument('--K', type=float, default=3, help='alpha')
+parser.add_argument('--isCal_SRF',type=bool, default=True,help='Yes means the SRF is not known and our method can adaptively learn it; No means the SRF is known as a prior information.')
+parser.add_argument('--isCal_PSF',type=bool, default=True,help='Yes means the PSF is not known and our method can adaptively learn it; No means the PSF is known as a prior information.')
+parser.add_argument('--pre_epoch', type=int, default=300, help='')
+
 
 opt = parser.parse_args()
