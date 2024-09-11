eadc02ab2173762a10b8c4024a3ee570c2f1a9ea
Xuheng Cao <caoxuheng@tongji.edu.cn>
2024-09-12 01:57:01 +0800

    Update config.py

--- Networks/ZSL/config.py
+++ Networks/ZSL/config.py
@@ -6,5 +6,6 @@ parser.add_argument('--msi_channel', type=int, default=1, help='msi channel numb
 parser.add_argument('--p', type=float, default=10, help='alpha')
 parser.add_argument('--init_lr1', type=float, default=1e-4, help='-')
 parser.add_argument('--init_lr2', type=float, default=5e-4, help='=')
+parser.add_argument('--decay_power', type=float, default=1.5, help='=')
 
 opt = parser.parse_args()
