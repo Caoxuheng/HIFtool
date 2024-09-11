24afee89548cec20626465369711d11f95386ac3
Xuheng Cao <caoxuheng@tongji.edu.cn>
2024-09-12 01:55:05 +0800

    Update config.py

--- Networks/ZSL/config.py
+++ Networks/ZSL/config.py
@@ -4,5 +4,7 @@ parser.add_argument('--sf', type=int, default=4, help="super resolution upscale 
 parser.add_argument('--hsi_channel', type=int, default=4, help='output channel number')
 parser.add_argument('--msi_channel', type=int, default=1, help='msi channel number')
 parser.add_argument('--p', type=float, default=10, help='alpha')
+parser.add_argument('--init_lr1', type=float, default=1e-4, help='-')
+parser.add_argument('--init_lr2', type=float, default=5e-4, help='=')
 
 opt = parser.parse_args()
