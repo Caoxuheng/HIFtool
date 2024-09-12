82d8a7595c19c46ba005ffec62b595bddbfdaea6
Xuheng Cao <caoxuheng@tongji.edu.cn>
2024-09-12 10:51:20 +0800

    Update net.py

--- Networks/ZSL/net.py
+++ Networks/ZSL/net.py
@@ -1,3 +1,21 @@
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True' 
+from model1 import *
+
+
+PSF = fspecial('gaussian', 7, 3)
+p=10
+
+
+LR=1e-3
+
+loss_optimal=1.75
+
+import imgvision
+import numpy as np
+from .Model.cnn import ZSL_cnn
+from .utils import  *
 class ZSL():
     def __init__(self,args):
         self.args = args
@@ -5,8 +23,38 @@ class ZSL():
         self.model = ZSL_cnn(args)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=LR, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
         self.equip_degradation(args)
-    def equip_degradation(self,args):
-        pass
+        
+    def degradation_est(self,lhsi,hmsi,size_B):
+        B,L,M,N = hmsi.shape
+        R = np.ones(self.args.msi_channel,self.args.hsi_channel);
+        R /= self.args.hsi_channel
+
+        HSI_2D = lhsi.reshape([-1,self.args.hsi_channel])
+        s0 = 1
+        iter = 20
+        B = np.abs(np.random.randn(size_B, size_B));
+
+        mu = 0
+        B /= B.sum()
+        for i in range(iter):
+            fft_B = psf2otf(B, [M N]);
+
+        MSI_BS = Gaussian_downsample(hmsi, fft_B, self.args.sf, s0)
+        MSI_BS = MSI_BS.reshape([-1,self.args.msi_channel])
+        R = R_update2(MSI_BS, HSI_2D, R, mu)
+
+        R_HSI = R * HSI_2D
+        R_HSI =R_HSI.reshape(lhsi.shape)
+        R_HSI_up = zeros(M, N, L)
+        R_HSI_up[s0:: self.args.sf, s0::self.args.sf]=R_HSI
+        B = B_update2(R_HSI_up, hmsi, size_B, self.args.sf, B, mu)
+        return R,B
+
+
+    def equip_degradation(self,args,lhsi,hmsi,size_):
+        self.degradation_est(lhsi,hmsi,size_B)
+        self.spadown =
+        self.spedown = 
 
 
     def get_traindata(self,lhsi,hmsi,U):
@@ -68,3 +116,30 @@ class ZSL():
                 self.optimizer.step()
 
         return  Xre[0].detach().cpu().numpy().T
+
+cnn=CNN(args).cuda()
+
+for epoch in range(EPOCH): 
+    for step1, (a1, a2,a3) in enumerate(train_loader): 
+        cnn.train()
+        lr=warm_lr_scheduler(optimizer, init_lr1,init_lr2, warm_iter,step, lr_decay_iter=1,  max_iter=maxiteration, power=decay_power)
+        step=step+1
+        output,Xre= cnn(a3.cuda(),a2.cuda())
+
+        loss = loss_func(Xre, a1.cuda(),a2.cuda(),downsample_factor)
+        optimizer.zero_grad()           
+        loss.backward()               
+        optimizer.step()
+    cnn.eval()
+    with torch.no_grad():
+        abudance = cnn(HSI_1.cuda(),MSI_1.cuda())
+        abudance=abudance.cpu().detach().numpy()
+        abudance1=np.squeeze(abudance)
+    Fuse2=np.tensordot(U0, abudance1, axes=([1], [0]))
+    sum_loss,psnr_=metrics.rmse1(np.clip(Fuse2,0,1),HRHSI)
+    if sum_loss<loss_optimal:
+       loss_optimal=sum_loss
+    loss_list.append(sum_loss)
+    print(epoch,lr,sum_loss,psnr_)
+    torch.save(cnn.state_dict(), data2+'1.pkl',_use_new_zipfile_serialization=False) 
+
