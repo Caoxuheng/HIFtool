28f374c97d7d7636297d43871f2969d541cd895c
Xuheng Cao <caoxuheng@tongji.edu.cn>
2024-09-12 09:51:36 +0800

    Update config.py

--- Networks/ZSL/config.py
+++ Networks/ZSL/config.py
@@ -7,5 +7,6 @@ parser.add_argument('--p', type=float, default=10, help='alpha')
 parser.add_argument('--init_lr1', type=float, default=1e-4, help='-')
 parser.add_argument('--init_lr2', type=float, default=5e-4, help='=')
 parser.add_argument('--decay_power', type=float, default=1.5, help='=')
+parser.add_argument('--BATCH_SIZE', type=int, default=64, help='=')
 
 opt = parser.parse_args()
