7268dd9757420ecad4ae29fe2d19d4bd34f64e53
Xuheng Cao <caoxuheng@tongji.edu.cn>
2025-02-13 11:16:19 +0800

    Update Network_training_VL.py

--- Network_training_VL.py
+++ Network_training_VL.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 import numpy as np
 
-def train(model,training_data_loader, validate_data_loader,model_folder,optimizer,lr,start_epoch=0,end_epoch=2000,ckpt_step=50,RESUME=False):
+def train(model,training_data_loader, validate_data_loader,model_folder,optimizer,lr,start_epoch=0,end_epoch=2000,ckpt_step=50,RESUME=False,meta=False):
     PLoss=nn.L1Loss()
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=100,
                                                    gamma=0.5)
@@ -65,6 +65,7 @@ def train(model,training_data_loader, validate_data_loader,model_folder,optimize
                     psnr.append(PSNR_GPU(output_HRHSI, GT).item())
             v_loss = np.nanmean(np.array(epoch_val_loss))
             psnr = np.nanmean(np.array( psnr))
+
             if psnr>best:
                 best=psnr
                 best_epoch=epoch
@@ -73,12 +74,16 @@ def train(model,training_data_loader, validate_data_loader,model_folder,optimize
             print("             learning rate:º%f" % (optimizer.param_groups[0]['lr']))
             print('             validate loss: {:.7f}'.format(v_loss))
             print('             PSNR loss: {:.7f}'.format(psnr ))
+
     return best_epoch
+
+
 def meta_train(model, training_data_loader):
     # ============Epoch Train=============== #
     for iteration, batch in enumerate(training_data_loader, 1):
         GT, LRHSI, HRMSI = batch[0].cuda(), batch[1].cuda(), batch[2].cuda()
         model(GT,iteration)
+
 
 
 def evalu_model_specific(model,opt,model_folder,test_epoch,end_epoch,dataset_name):
@@ -87,12 +92,12 @@ def evalu_model_specific(model,opt,model_folder,test_epoch,end_epoch,dataset_nam
     from einops import rearrange
     from data import SpaDown,getInputImgs,SpeDown
 
-    patch_size = 256
+    patch_size = 512
     save_folder =model_folder+ dataset_name +'/'
     if not os.path.isdir(save_folder):
         os.mkdir(save_folder)
-    # path_checkpoint = model_folder + "{}.pth".format(test_epoch)
-    # checkpoint = torch.load(path_checkpoint)
+    path_checkpoint = model_folder + "{}.pth".format(test_epoch)
+    checkpoint = torch.load(path_checkpoint)
 
 
     R = torch.tensor(np.load(opt.srfpath)).float().cuda()
@@ -103,13 +108,16 @@ def evalu_model_specific(model,opt,model_folder,test_epoch,end_epoch,dataset_nam
         lst=[ 74, 62, 71, 70, 57, 66, 75, 2, 35, 34, 23, 59, 29, 65, 28, 8, 43, 63, 42, 19, 13, 33,
                              20,36, 9]
         size = 1024
-    lx = [49.21,36.04,52.06]
+    lx = [0,0,0,0,0]
     for ax, idx in enumerate(lst):
         mx=0
         model.load_state_dict(checkpoint['net'])
         model = model.cuda()
-        Spatialdown = SpaDown(opt.sf)
-        LRHSI, HRMSI, GT_ = getInputImgs(opt,dataset_name, idx, Spatialdown)
+        Spatialdown = SpaDown(opt.sf, predefine=None)
+
+        LRHSI, HRMSI, GT_ = getInputImgs(opt, idx, Spatialdown)
+
+        # LRHSI, HRMSI, GT_ = getInputImgs(opt,dataset_name, idx, Spatialdown)
         GT = torch.FloatTensor(GT_).T.unsqueeze(0).cuda()
         LRHSI,HRMSI = LRHSI.cuda(), HRMSI.cuda()
         optimizer = torch.optim.Adam(model.parameters(),lr = 3e-5)
@@ -155,76 +163,6 @@ def evalu_model_specific(model,opt,model_folder,test_epoch,end_epoch,dataset_nam
                     save_checkpoint(save_folder, model, optimizer, lr, idx)
 
 
-def evalu_model_specific(model,opt,model_folder,test_epoch,end_epoch,dataset_name):
-    import imgvision as iv
-    import os
-    from einops import rearrange
-    from data import SpaDown,getInputImgs,SpeDown
-
-    patch_size = 512
-    save_folder =model_folder+ dataset_name +'/'
-    if not os.path.isdir(save_folder):
-        os.mkdir(save_folder)
-
-
-    R = torch.tensor(np.load(opt.srfpath)).float()
-    if dataset_name.lower()=='cave':
-        lst =[1,2,7,8,9,10,11,15,19,23,26,27]
-        size = 512
-    elif dataset_name.lower() =='harvard':
-        lst=[ 74, 62, 71, 70, 57, 66, 75, 2, 35, 34, 23, 59, 29, 65, 28, 8, 43, 63, 42, 19, 13, 33,
-                             20,36, 9]
-        size = 1024
-    lx = [49.21,36.04,52.06]
-    for ax, idx in enumerate(lst):
-        mx=0
-        # model.load_state_dict(checkpoint['net'])
-        model = model
-        Spatialdown = SpaDown(opt.sf)
-        LRHSI, HRMSI, GT_ = getInputImgs(opt,dataset_name, idx, Spatialdown)
-        GT = torch.FloatTensor(GT_).T.unsqueeze(0)
-        LRHSI,HRMSI = LRHSI, HRMSI
-        optimizer = torch.optim.Adam(model.parameters(),lr = 3e-5)
-        PixelLoss=nn.L1Loss()
-        Spatialdown = Spatialdown
-        LR,HR,GTs=[],[],[]
-
-        for i in range(size//patch_size):
-            for j in range(size//patch_size):
-                LR.append(LRHSI[:,:,i*patch_size//32:(i+1)*patch_size//32,j*patch_size//32:(j+1)*patch_size//32])
-                HR.append(HRMSI[:, :, i * patch_size:(i + 1)  * patch_size ,j * patch_size:(j + 1)  * patch_size])
-                GTs.append(GT[:, :, i * patch_size:(i + 1)  * patch_size, j * patch_size:(j + 1)  * patch_size])
-
-        LR = torch.cat(LR,dim=0)
-        HR = torch.cat(HR, dim=0)
-        GTs= torch.cat(GTs, dim=0)
-
-        for epoch in range(end_epoch):
-            for b in range(2):
-                # LR_,HR_ = LR[b*2:(b+1)*2],HR[b*2:(b+1)*2]
-                # print(LR_.shape,HR_.shape)
-                optimizer.zero_grad()
-                Pred = model(LR,HR)
-                Predlr = Spatialdown(rearrange(Pred,'(s b) c h w -> s (b c) h w', s=1))
-                Predlr = rearrange(Predlr,' s (b c) h w -> (s b) c h w ',c=LR.shape[1])
-                Prehr = SpeDown(Pred,R)
-                loss = PixelLoss(Predlr,LR)+PixelLoss(Prehr,HR)
-                loss.backward()
-                optimizer.step()
-            if epoch%10==0:
-                model.eval()
-                with torch.no_grad():
-                    Pred = model(LR,HR)
-                pr = PSNR_GPU(GTs, Pred).detach().cpu().numpy()
-                print('\r{} Epoch: {}\tLoss: {:.4f}\tPSNR: {:.3f}\t Best PSNR:{:.3f}'.format(idx,epoch,loss.detach().cpu().numpy(),pr,mx), end='')
-                if pr>mx and epoch>40:
-                    Re = reshuffle(Pred,patch_size)
-                    # iv.spectra_metric(Re,GT_).Evaluation()
-                    # plt.imshow(iv.spectra().space(Re))
-                    # plt.show()
-                    np.save(save_folder+str(idx), Re)
-                    mx=pr
-                    save_checkpoint(save_folder, model, optimizer, lr, idx)
 
 
 if __name__=='__main__':
@@ -232,28 +170,28 @@ if __name__=='__main__':
     from torch.utils.data import DataLoader
 
     # Build Network
-    Method = 'UALT'
+    Method = 'UTAL'
+
     model, opt = model_generator(Method,'cuda')
 
-    dataset_name = 'cave'
+    dataset_name = 'harvard'
     model_folder = Method + '/' + dataset_name + '/'
 
     # Training Setting
-    Batch_size = 4
+    Batch_size = 2
     end_epoch = 2000
     ckpt_step = 50
     lr = 1e-4
-    patch_size = 256 #Crop Ps
+    patch_size = 512 #Crop Ps
 
     # Scheme Setting
     General=True
-    Meta = False
+    bestepoch=550
+    meta = False
     Specific=False
 
-    #  pre-train config
-    bestepoch=0
     resume=False
-    start=0
+    start=700
 
     if General:
         from Dataloader_tool import Large_dataset
@@ -264,14 +202,18 @@ if __name__=='__main__':
                                       pin_memory=True, drop_last=False)
         validate_data_loader = DataLoader(dataset=Val_data, num_workers=0, batch_size=Batch_size, shuffle=True,
                                           pin_memory=True, drop_last=True)
-        optimizer = torch.optim.Adam(model.parameters(),lr=lr)
-        bestepoch = train(model,training_data_loader,validate_data_loader,model_folder=model_folder,optimizer=optimizer,lr=lr,start_epoch=start,end_epoch=end_epoch,ckpt_step=ckpt_step,RESUME=resume)
-    if Meta:
-        Train_data = Large_dataset(opt, patch_size,dataset_name ,type='test')
-       
-        training_data_loader = DataLoader(dataset=Train_data, num_workers=0, batch_size=1, shuffle=True,
-                                      pin_memory=True, drop_last=False)
-         meta_train(model, training_data_loader)
-        
+        if meta:
+            Train_data = Large_dataset(opt, patch_size, dataset_name, type='test')
+
+            training_data_loader = DataLoader(dataset=Train_data, num_workers=0, batch_size=Batch_size, shuffle=True,
+                                              pin_memory=True, drop_last=False)
+
+            meta_train(model, training_data_loader)
+
+        else:
+            optimizer = torch.optim.Adam(model.parameters(),lr=lr)
+            bestepoch = train(model,training_data_loader,validate_data_loader,model_folder=model_folder,optimizer=optimizer,lr=lr,start_epoch=start,end_epoch=end_epoch,ckpt_step=ckpt_step,RESUME=resume,meta = meta)
+
     if Specific:
         evalu_model_specific(model, opt, model_folder, bestepoch, end_epoch, dataset_name)
+
