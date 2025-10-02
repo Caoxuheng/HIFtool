e928986ae992382b6cf934957bb0adc87382a39f
Xuheng Cao <caoxuheng@tongji.edu.cn>
2025-10-02 23:33:40 +0800

    Create Config.py

---
+++ Networks/CaFormer/Config.py
@@ -0,0 +1,11 @@
+import argparse
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--sf',type=int,default=32, help='scale factor')
+
+parser.add_argument('--inchannel',type=int,default=31, help='spectral band number')
+parser.add_argument('--patch_size',type=int,default=80, help='patch size for training')
+
+
+parser.add_argument('--n_depth',type=int,default=3, help='depth number of the autoencoder embedded in prior learning module')
+parser.add_argument('--n_feat',type=int,default=64, help='feature number of the autoencoder')
+args=parser.parse_args()
