0cc4b63a72e0e1870e1dc7366a9a3302b9fbbea2
Xuheng Cao <caoxuheng@tongji.edu.cn>
2025-10-09 00:05:34 +0800

    Update Config.py

--- Networks/CaFormer/Config.py
+++ Networks/CaFormer/Config.py
@@ -1,8 +1,8 @@
 import argparse
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--sf',type=int,default=32, help='scale factor')
-
-parser.add_argument('--msi_channel',type=int,default=31, help='spectral band number')
+parser.add_argument('--msi_channel',type=int,default=3, help='MSI spectral band number')
+parser.add_argument('--hsi_channel',type=int,default=31, help='HSI spectral band number')
 parser.add_argument('--patch_size',type=int,default=80, help='patch size for training')
 
 
