5501cbc793202335e3fce5e9113b4f5e87314546
Xuheng Cao <caoxuheng@tongji.edu.cn>
2025-10-21 12:56:32 +0800

    Update config.py

--- Networks/HySure/config.py
+++ Networks/HySure/config.py
@@ -4,4 +4,9 @@ parser.add_argument('--sf',type=int,default=32, help='scale_factor, set to 8, 16
 parser.add_argument("--lam_p", type=float, default=0.05,help='')
 parser.add_argument("--lam_r", type=float, default=5e-5,help='')
 parser.add_argument("--lam_m", type=float, default=1,help='')
+parser.add_argument("--hsi_channel", type=int, default=31,help='')
+parser.add_argument("--msi_channel", type=int, default=3,help='')
+
+parser.add_argument("--isCal_SRF", type=bool, default=False,help='')
+parser.add_argument("--isCal_PSF", type=bool, default=True,help='')
 args=parser.parse_args()
